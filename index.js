16c3146a41b5fe36aab8fb30a991edcae22e1320
phumthai <phumthai.spy@hotmail.com>
2021-10-08 14:10:29 +0700

    first push

---
+++ index.js
@@ -0,0 +1,26 @@
+var snmp = require ("net-snmp");
+
+var session = snmp.createSession ("10.71.0.3", "cmumrtg");
+
+var oids = ["1.3.6.1.4.1.14179.2.2.2.1.4"];
+
+session.get (oids, function (error, varbinds) {
+    if (error) {
+        console.error (error);
+    } else {
+        for (var i = 0; i < varbinds.length; i++) {
+            if (snmp.isVarbindError (varbinds[i])) {
+                console.error (snmp.varbindError (varbinds[i]));
+            } else {
+                console.log (varbinds[i].oid + " = " + varbinds[i].value);
+            }
+        }
+    }
+    session.close ();
+});
+
+session.trap (snmp.TrapType.LinkDown, function (error) {
+    if (error) {
+        console.error (error);
+    }
+});
