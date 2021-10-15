7d56751dd09d60bff04c76e4d1b1955d0da72546
phumthai <phumthai.spy@hotmail.com>
2021-10-15 13:40:46 +0700

    get ap name and channal

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
+const { get } = require("http");
 var snmp = require ("net-snmp");
 
 var session = snmp.createSession ("10.71.0.3", "cmumrtg");
 
-// var oids = "1.3.6.1.4.1.14179.2.2.1.1.3"; // AP name
-var oids = "1.3.6.1.4.1.14179.2.2.2.1.4"; // Wireless channal
+var oid_ap = "1.3.6.1.4.1.14179.2.2.1.1.3"; // AP name
+var oid_ch = "1.3.6.1.4.1.14179.2.2.2.1.4"; // Wireless channal
 
-// session.get (oids, function (error, varbinds) {
-//     if (error) {
-//         console.error (error);
-//     } else {
-//         for (var i = 0; i < varbinds.length; i++) {
-//             if (snmp.isVarbindError (varbinds[i])) {
-//                 console.error (snmp.varbindError (varbinds[i]));
-//             } else {
-//                 console.log (varbinds[i].oid + " = " + varbinds[i].value);
-//             }
-//         }
-//     }
-//     session.close ();
-// });
-
-// session.trap (snmp.TrapType.LinkDown, function (error) {
-//     if (error) {
-//         console.error (error);
-//     }
-// });
-// var store = snmp.createModuleStore ();
-// store.loadFromFile ("mib/bsnwras.my");
-// var providers = store.getProvidersForModule ("SNMPv2-MIB");
-// agent.getMib ().registerProviders (providers);
+var apn_oid = [];
+var wch_oid = [];
 
 function doneCb (error) {
     if (error)
         console.error (error.toString ());
 }
 
-function feedCb (varbinds) {
+function feedCb_apn (varbinds) {
     for (var i = 0; i < varbinds.length; i++) {
         if (snmp.isVarbindError (varbinds[i]))
             console.error (snmp.varbindError (varbinds[i]));
         else
-            console.log (varbinds[i].oid + "|" + varbinds[i].value);
+            //console.log (varbinds[i].oid + "|" + varbinds[i].value);
+            var x = [];
+            var z = varbinds[i].oid;
+            z = z.replace('1.3.6.1.4.1.14179.2.2.1.1.3.','');
+            var y;
+            x.push(z);
+            y = "|" + varbinds[i].value;
+            y = y.substring(1);
+            x.push(y);
+            apn_oid.push(x);
+    }
+}
+
+function feedCb_wch (varbinds) {
+    for (var i = 0; i < varbinds.length; i++) {
+        if (snmp.isVarbindError (varbinds[i]))
+            console.error (snmp.varbindError (varbinds[i]));
+        else
+            //console.log (varbinds[i].oid + "|" + varbinds[i].value);
+            var x = [];
+            var z = varbinds[i].oid;
+            z = z.replace('1.3.6.1.4.1.14179.2.2.2.1.4.','');
+            var y;
+            x.push(z);
+            y = "|" + varbinds[i].value;
+            y = y.substring(1);
+            x.push(y);
+            wch_oid.push(x);
     }
 }
 
@@ -48,4 +54,31 @@ var maxRepetitions = 20;
 
 // The maxRepetitions argument is optional, and will be ignored unless using
 // SNMP verison 2c
-session.subtree (oids, maxRepetitions, feedCb, doneCb);
+
+async function first(){
+    session.subtree (oid_ap, maxRepetitions, feedCb_apn, doneCb);
+    session.subtree (oid_ch, maxRepetitions, feedCb_wch, doneCb);
+}
+
+async function second(){
+    //console.log(apn_oid.length);
+    var x = wch_oid[0][0];
+    x = x.slice(0,-2)
+    console.log(x);
+}
+
+async function main(){
+    await first();
+    await second();
+}
+first().then(()=>{
+    setTimeout(function(){
+        second();
+    },2000)
+})
+// setTimeout(function(){
+    
+// },2000);
+
+
+
